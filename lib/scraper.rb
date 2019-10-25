ebf3ea548700bf04fb1906ef14baeefe3101da02
Riley Moore <rilemoore@gmail.com>
2019-10-25 19:21:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-doc.css("h1").text.strip
+puts doc.css("h1").text.strip
 
 # courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].name
 # puts doc.css(".headline-260IBN")
